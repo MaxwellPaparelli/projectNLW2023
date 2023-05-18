972776af3feb985a42258b93f1601e65ad693819
Maxwell Avila Paparelli <72114213+MaxwellPaparelli@users.noreply.github.com>
2023-05-18 00:12:45 +0000

    aula 2 parte 1

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,25 @@
 import Image from 'next/image'
 export default function Home() {
   return (
-  <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-    <h1 className="text-4xl font-bold">Sua capsula do tempo</h1>
-    <h1 className="text-4xl font-bold font-alt">Sua capsula do tempo</h1>
-  </div>)
+    <main className='grid grid-cols-2 min-h-screen '>
+      {/*Left*/}
+      <div className='relative fex flex-col items-start justify-between px-28 py-16 overflow-hidden border-r border-white/10 bg-[url(../assents/bg-stars.svg)] bg-cover'>
+        {/*Blur*/}
+        <div className='absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full'></div>
+        {/*Stipes*/}
+        <div className='absolute right-2 top-0 bottom-0 w-2 bg-stripes'></div>
+      </div>
+
+      {/*Right*/}
+      <div className='flex flex-col p-16 bg-[url(../assents/bg-stars.svg)] bg-cover'>
+        <div className='flex flex-1 items-center justify-center'>
+          <p className='text-center leading-relaxed w-[360px]'>
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className='Underline hover:text-gray-50'>criar agora!</a>
+          </p>
+        </div>
+        
+      </div>
+    </main>
+  )
 }
